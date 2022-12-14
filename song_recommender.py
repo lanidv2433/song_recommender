62a2043dc21102c44cb64ef4e59d4cb820decd3d
Lanni <63504496+lanidv2433@users.noreply.github.com>
2022-12-14 17:35:02 -0500

    Update song_recommender.py

--- song_recommender.py
+++ song_recommender.py
@@ -3,6 +3,8 @@ import spotipy
 from credentials import client_ID, client_SECRET
 from spotipy.oauth2 import SpotifyClientCredentials
 
+# connects to Spotify API client using Spotipy library
+
 credentials = SpotifyClientCredentials(client_id=client_ID, client_secret=client_SECRET)
 sp = spotipy.Spotify(client_credentials_manager=credentials)
 
@@ -12,22 +14,23 @@ def run():
         title = input("Enter the title of a song: ")
         artist = input("Enter the artist the song is by: ")
 
-        results = sp.search(q='artist:' + artist + ', track:' + title, limit = 15, type="track")
+        results = sp.search(q='artist:' + artist + ', track:' + title, limit = 15, type="track") # finds the possible songs that match the user input from the Spotify library
         
         if results['tracks']['total'] == 0:
             print("No results found. Please make sure the song title and artist are correct. Please try again.\n")
         else:
             print(f"Here are some songs similar to {title} by {artist}:")
-            song = results['tracks']['items'][0]
+            song = results['tracks']['items'][0] # saves the data of the user inputted song 
             
 
-            recommendations = sp.recommendations(seed_tracks=[song['uri']])
+            recommendations = sp.recommendations(seed_tracks=[song['uri']]) # extracts the uri of the user inputted song and uses it to fuel recommendations
             recDic = {}
             for num, track in enumerate(recommendations['tracks'], 1):
                 print(f"{num}. {track['name']} - {track['artists'][0]['name']}")
               
-                recDic[num] = [track['preview_url'], f"{track['name']} by {track['artists'][0]['name']}"]
-
+                recDic[num] = [track['preview_url'], f"{track['name']} by {track['artists'][0]['name']}"] # saves the preview url and the title and artist of each song
+            
+            # UI menu vvv
             endAgain = True
             while endAgain:
                 songNum = int(input("Enter the number of the song you'd like to preview (0 for no preview): "))
